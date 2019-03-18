de65b3927e6cd5c0a9502f6d2f3d68e7d2700d21
Padna <coryf175@gmail.com>
2019-03-18 17:19:48 +0000

    Messaging is now functioning

--- WebChat/wwwroot/js/chat.js
+++ WebChat/wwwroot/js/chat.js
@@ -2,20 +2,31 @@
 
 connection.on("ReceiveMessage", function (user, message) {
     var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
-    var encodedMsg = user + " says " + msg;
+    var encodedMsg = user + ": " + msg;
     var li = document.createElement("li");
     li.textContent = encodedMsg;
     document.getElementById("messages").appendChild(li);
 });
 
-connection.start().then(function () {
-    let input = document.getElementById('input_box');
-    input.onkeypress = function (ev) {
-        if (ev.which === 13) {
-            connection.invoke("SendMessage", "rdmUser", input.value);
-            input.value = '';
-        }
-    }
+connection.start().then(() => {
+   
 }).catch(function (err) {
     return console.error(err.toString());
     });
+
+function listenForMessage(usr) {
+    let input = document.getElementById('input_box');
+    input.onkeypress = function (ev) {
+        if (ev.which === 13) {
+            if(usr === null) {
+                let li = document.createElement("li");
+                li.textContent = "You must be logged in to send a chat message!";
+                document.getElementById("messages").appendChild(li);
+                return;
+            }
+            console.log(usr);
+            connection.invoke("SendMessage", usr.textContent, input.value);
+            input.value = '';
+        }
+    }
+}
